8770016186a253114ed64f875f77e69c847b3ff6
stefano <galarragas@gmail.com>
2013-11-04 22:17:59 +0000

    Incomplete test. Still not compiling but need to save data from this computer

--- examples/src/test/scala/com/pragmasoft/scaldingunit/sample/functional/SampleJobTest.scala
+++ examples/src/test/scala/com/pragmasoft/scaldingunit/sample/functional/SampleJobTest.scala
@@ -2,11 +2,18 @@ package com.pragmasoft.scaldingunit.sample.functional
 
 import org.scalatest.matchers.ShouldMatchers
 import org.scalatest.FlatSpec
-import com.twitter.scalding.{Tsv, Osv, JobTest}
+import com.twitter.scalding._
 import cascading.tuple.Tuple
 import com.pragmasoft.scaldingunit.sample.SampleJob
+import com.pragmasoft.scaldingunit.sample.SampleJobPipeTransformations._
+import scala.collection.mutable
+import com.twitter.scalding.Tsv
+import com.twitter.scalding.Osv
 
-class SampleJobTest extends FlatSpec with ShouldMatchers {
+class SampleJobTest extends FlatSpec with ShouldMatchers with FieldConversions with TupleConversions {
+
+  val eventData = List( ("12/02/2013 10:22:11", 1000002l, "http://www.youtube.com") )
+  val userData = List( (1000002l, "stefano@email.com", "10 Downing St. London") )
 
   "A sample job" should "do the full transformation" in {
 
@@ -14,9 +21,12 @@ class SampleJobTest extends FlatSpec with ShouldMatchers {
       .arg("eventsPath", "eventsPath")
       .arg("userInfoPath", "userInfoPath")
       .arg("outputPath", "outputPath")
-      .source[Tuple](Osv("eventsPath", MPOD_INPUT_SCHEMA), mpodData)
-      .source[Tuple](Osv("userInfoPath"), registeredUserData)
-      .sink[Tuple](Tsv("outputPath", MPOD_AMENDED_ID_SCHEMA))(assertion)
+      .source(Osv("eventsPath", INPUT_SCHEMA), eventData)
+      .source(Osv("userInfoPath", USER_DATA_SCHEMA), userData)
+      .sink[Tuple](Tsv("outputPath", OUTPUT_SCHEMA)) {
+            buffer: mutable.Buffer[Tuple] =>
+              buffer.toList shouldEqual List( new Tuple("2013/02/11", 1000002l, "stefano@email.com", "10 Downing St. London", 1l) )
+          }
       .run
       .finish
   }
